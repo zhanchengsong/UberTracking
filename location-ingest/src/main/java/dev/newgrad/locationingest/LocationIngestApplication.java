961342651b331dc096628403c210e83bde6a7256
Zhancheng Song <zhanchengsong@Zhanchengs-MacBook-Pro.local>
2019-08-21 23:26:28 -0400

    Created location ingest service for intake location update and generate messages

---
+++ location-ingest/src/main/java/dev/newgrad/locationingest/LocationIngestApplication.java
@@ -0,0 +1,13 @@
+package dev.newgrad.locationingest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationIngestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationIngestApplication.class, args);
+    }
+
+}
---
+++ location-ingest/src/test/java/dev/newgrad/locationingest/LocationIngestApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.newgrad.locationingest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LocationIngestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
